e6eaf6bc01e5fa743c72664065cb3808b638638f
stuurdean <68418793+stuurdean@users.noreply.github.com>
2022-05-25 17:45:57 +0200

    Admin sign In

--- app/src/main/java/za/co/msocios/gotrackadmin/MainActivity.java
+++ app/src/main/java/za/co/msocios/gotrackadmin/MainActivity.java
@@ -2,13 +2,45 @@ package za.co.msocios.gotrackadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    public static int SPLASH=5000;
+    private FirebaseAuth auth;
+    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        auth = FirebaseAuth.getInstance();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                FirebaseUser currentUser = auth.getCurrentUser();
+                if (currentUser.isEmailVerified())
+                {
+                    //startActivity(new Intent());
+                }
+                else
+                {
+                    startActivity(new Intent(MainActivity.this,SignIn.class));
+                    finish();
+                }
+            }
+        },SPLASH);
     }
 }
--- app/src/main/java/za/co/msocios/gotrackadmin/SignIn.java
+++ app/src/main/java/za/co/msocios/gotrackadmin/SignIn.java
@@ -2,10 +2,24 @@ package za.co.msocios.gotrackadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.basgeekball.awesomevalidation.AwesomeValidation;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SignIn extends AppCompatActivity {
 
+    TextInputEditText email,password;
+    Button signIn,forgotPassword,signUp;
+    FirebaseAuth auth;
+    AwesomeValidation validation;
+    ProgressDialog progressDialog;
+    AlertDialog.Builder builder;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
