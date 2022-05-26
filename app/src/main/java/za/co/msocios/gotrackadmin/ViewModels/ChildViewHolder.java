87d8aa4020ef33effa61daa7f790291861327985
stuurdean <68418793+stuurdean@users.noreply.github.com>
2022-05-26 20:28:59 +0200

    Navigation

---
+++ app/src/main/java/za/co/msocios/gotrackadmin/ViewModels/ChildViewHolder.java
@@ -0,0 +1,18 @@
+package za.co.msocios.gotrackadmin.ViewModels;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class ChildViewHolder extends RecyclerView implements View.OnClickListener {
+    public ChildViewHolder(@NonNull Context context) {
+        super(context);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
