820881caaf04acec63b6d407cbe475024ce771b7
stuurdean <68418793+stuurdean@users.noreply.github.com>
2022-06-01 16:21:26 +0200

    Admin Sign In

--- app/src/main/java/za/co/msocios/gotrackadmin/ChildDetails.java
+++ app/src/main/java/za/co/msocios/gotrackadmin/ChildDetails.java
@@ -2,6 +2,7 @@ package za.co.msocios.gotrackadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 
 public class ChildDetails extends AppCompatActivity {
@@ -10,5 +11,10 @@ public class ChildDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_child_details);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+       // ActionBar actionBar = getActionBar();
+        //actionBar.setDisplayHomeAsUpEnabled(true);
+
+
     }
 }
