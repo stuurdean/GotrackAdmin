d5bb7c80fffea0be45cae79c2cebb7bf2149f40a
stuurdean <68418793+stuurdean@users.noreply.github.com>
2022-06-03 07:09:49 +0200

    Parent view layout

--- app/src/main/java/za/co/msocios/gotrackadmin/ChildDetails.java
+++ app/src/main/java/za/co/msocios/gotrackadmin/ChildDetails.java
@@ -3,9 +3,14 @@ package za.co.msocios.gotrackadmin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ChildDetails extends AppCompatActivity {
+
+    Button btnAssign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +19,17 @@ public class ChildDetails extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
        // ActionBar actionBar = getActionBar();
         //actionBar.setDisplayHomeAsUpEnabled(true);
+
+        btnAssign = findViewById(R.id.btndriver);
+
+
+        btnAssign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                startActivity(new Intent(ChildDetails.this,AssignDriverActivity.class));
+            }
+        });
 
 
     }
