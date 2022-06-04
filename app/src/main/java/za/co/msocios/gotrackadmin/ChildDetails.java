77b6d32c3da9c67cf5520c389df25becfebf56c4
stuurdean <68418793+stuurdean@users.noreply.github.com>
2022-06-05 00:45:28 +0200

    Driver Model

--- app/src/main/java/za/co/msocios/gotrackadmin/ChildDetails.java
+++ app/src/main/java/za/co/msocios/gotrackadmin/ChildDetails.java
@@ -7,10 +7,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ChildDetails extends AppCompatActivity {
 
     Button btnAssign;
+    TextView fullname,age,address,parentname,parentPhone,schoolName
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
---
+++ app/src/main/java/za/co/msocios/gotrackadmin/Common/Common.java
@@ -0,0 +1,10 @@
+package za.co.msocios.gotrackadmin.Common;
+
+import za.co.msocios.gotrackadmin.Models.Child;
+import za.co.msocios.gotrackadmin.Models.Parent;
+
+public class Common {
+
+    public  static Parent selectedParent;
+    public  static Child selectedChild;
+}
