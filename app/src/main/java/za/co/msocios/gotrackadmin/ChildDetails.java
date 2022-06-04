e23304fdd764028280d35f85a64cd05423c56f94
stuurdean <68418793+stuurdean@users.noreply.github.com>
2022-06-05 01:51:40 +0200

    Driver Model

--- app/src/main/java/za/co/msocios/gotrackadmin/ChildDetails.java
+++ app/src/main/java/za/co/msocios/gotrackadmin/ChildDetails.java
@@ -1,13 +1,22 @@
 package za.co.msocios.gotrackadmin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.ActionBar;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.squareup.picasso.Picasso;
 
 import za.co.msocios.gotrackadmin.Common.Common;
 import za.co.msocios.gotrackadmin.Models.Child;
@@ -17,6 +26,8 @@ public class ChildDetails extends AppCompatActivity {
     Button btnAssign;
     TextView fullname,age,address,parentname,parentPhone,schoolname,schoolAdress,grade,startTime,outTime,drivername;
     String docId ;
+    ImageView image;
+    FirebaseFirestore firestore = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,18 +52,44 @@ public class ChildDetails extends AppCompatActivity {
         outTime = findViewById(R.id.txt_out);
         drivername = findViewById(R.id.txt_driverName);
 
+        image = findViewById(R.id.childImageView);
+
 
         fullname.setText(Common.selectedChild.getFullNames());
         age.setText(Common.selectedChild.getDateOfBirth());
         address.setText(Common.selectedChild.getAddress());
-       // parentname.setText(Common.selectedChild.);
-       // parentPhone = findViewById(R.id.txt_parentPhone);
+        // parentname.setText(Common.selectedChild.);
+        // parentPhone = findViewById(R.id.txt_parentPhone);
         schoolname.setText(Common.selectedChild.getSchoolName());
         schoolAdress.setText(Common.selectedChild.getSchoolAddress());
         grade.setText(Common.selectedChild.getGrade());
         startTime.setText(Common.selectedChild.getInTime());
         outTime.setText(Common.selectedChild.getOutTime());
+
+
+        Picasso.get().load(Common.selectedChild.getImage()).into(image);
+
+
+
+
         drivername.setText(Common.selectedChild.getDriver());
+
+
+
+        firestore.collection("Parents").document(docId).get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        DocumentSnapshot document = task.getResult();
+
+                        if (document.exists())
+                        {
+                            parentname.setText(document.get("names").toString());
+                            parentPhone.setText(document.get("phoneNumber").toString());
+                        }
+                    }
+                });
+
 
 
         btnAssign.setOnClickListener(new View.OnClickListener() {
